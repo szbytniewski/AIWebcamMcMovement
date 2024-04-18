8347b219a8d79f9fa8f4f71671b071ddfee823b1
szbytniewski <s.zbytniewski.624@studms.ug.edu.pl>
2024-04-18 22:37:51 +0200

    Added opening inventory eating and single left clicking(last one needs some work b/c doesn't work properly)

--- poseDetecMovment.py
+++ poseDetecMovment.py
@@ -43,7 +43,9 @@ first_nose_x, first_nose_y = 0,0
 wrist_above_shoulder = False
 mining_state = False
 state = "down"
-first_nose_detected = False 
+first_nose_detected = False
+e_cliecked = False
+left_clicked = False
 
 # Other variables 
 step_threshold = 0.005         
@@ -79,27 +81,74 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 
             # jumping
             curr_right_shoulder_y = landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y
-            if (curr_right_shoulder_y > prev_right_shoulder_y + 0.04 ):
-                pydirectinput.keyDown("space")
-                # print("jump")
-            else:
-                pydirectinput.keyUp("space")
-
-            # placing blocks
-            prev_right_shoulder_y = curr_right_shoulder_y
-            if(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y > landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y):
-                wrist_above_shoulder = True
-                if wrist_above_shoulder:
-                    pydirectinput.rightClick()
-                    # print("placing blocks")
-            else:
-                wrist_above_shoulder = False
 
             # mining
             wristR = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y] 
             elbowR = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y] 
             shoulderR = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y] 
 
+            # walking
+            ankleR = landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y
+            ankleL = landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y
+
+            # movement
+            nose_x = int(landmarks[mp_pose.PoseLandmark.NOSE.value].x * image_width)
+            nose_y = int(landmarks[mp_pose.PoseLandmark.NOSE.value].y * image_height)
+
+            # placing
+            shoulderL = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            wristL = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y] 
+
+            # left click right hold and open inventory (some of the points already called only added the ones that weren't)
+            hipR = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+            hipL = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x, landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y] 
+
+            right_hand_in_trapezoid = (wristR[0] > shoulderR[0] and wristR[0] > hipR [0] and wristR[1] > shoulderR[1] and wristR[1] < hipR[1])
+            left_hand_in_trapezoid = (wristL[0] < shoulderL[0] and wristL[0] < hipL [0] and wristL[1] > shoulderL[1] and wristL[1] < hipL[1])
+
+            # opening inventory eating and single left clicking
+            if (left_hand_in_trapezoid and right_hand_in_trapezoid):
+                if not e_cliecked:
+                    pydirectinput.press("e")
+                    # print("open inventory")
+                    e_cliecked = True
+            elif (left_hand_in_trapezoid):
+                pydirectinput.mouseDown(button='right')
+                print("eating")
+            elif (right_hand_in_trapezoid):
+                if not left_clicked:
+                    # doesn't work for some reason i don't know why
+                    pydirectinput.mouseDown(button='left')
+                    print("single click")
+                    left_clicked = True
+            else:
+                pydirectinput.mouseUp(button='right')
+                pydirectinput.mouseUp(button='left')
+                if e_cliecked == True:
+                    e_cliecked = False
+                if left_clicked == True:
+                    left_clicked = False
+
+            # jumping condition
+            if (curr_right_shoulder_y > prev_right_shoulder_y + 0.04 ):
+                pydirectinput.keyDown("space")
+                # print("jump")
+            else:
+                pydirectinput.keyUp("space")
+                pass
+ 
+            # placing blocks condition
+            prev_right_shoulder_y = curr_right_shoulder_y
+            if(shoulderL[1] > wristL[1]):
+                wrist_above_shoulder = True
+                if wrist_above_shoulder:
+                    pydirectinput.rightClick()
+                    # print("placing blocks")
+                    # pass
+            else:
+                wrist_above_shoulder = False
+
+            # mining condition
             angle = calculate_angle(shoulderR, elbowR, wristR)
 
             if angle < 30:
@@ -118,24 +167,25 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 
             if mining_state:
                 pydirectinput.mouseDown()
-                print("Mining action")
+                # print("Mining action")
+                # pass
             else:
                 pydirectinput.mouseUp()
-                pass
+                # pass
 
-            # Walking
-            ankleR = landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y
-            ankleL = landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y
 
+            # walking condition
             if ankleL > prev_right_ankle_y + step_threshold and ankleR < prev_left_ankle_y - step_threshold:
                 last_movment_instace = time.time()
                 pydirectinput.keyDown('ctrl')
                 pydirectinput.keyDown('w')
                 # print("Step forward detected")
+                # pass
 
             if time.time() - last_movment_instace > motion_timeout:
                 pydirectinput.keyUp('ctrl')
                 pydirectinput.keyUp('w')
+                # pass
 
 
             prev_right_ankle_y = ankleL
@@ -146,16 +196,12 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
                     )
             
-            nose_x = int(landmarks[mp_pose.PoseLandmark.NOSE.value].x * image_width)
-            nose_y = int(landmarks[mp_pose.PoseLandmark.NOSE.value].y * image_height)
 
+            # movement condition
             if not first_nose_detected:
                 first_nose_x = nose_x
                 first_nose_y = nose_y
                 first_nose_detected = True
-                print(first_nose_x, first_nose_y)
-                print(first_nose_x + 50, first_nose_y + 50)
-                print(first_nose_x - 50, first_nose_y - 50)
 
             cursor_dx = 0
             cursor_dy = 0
